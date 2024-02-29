8967881056f0d0dac8c1e35b4e61c16335a74512
arctan95 <jaeqentan@gmail.com>
2024-03-01 02:15:00 +0800

    A minimal 64-bit kernel for the x86 architecture

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,18 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
     loop {}
 }
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
